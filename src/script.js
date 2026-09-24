6a37fd47f2b75b64a7b2c010d5a1f510332042e0
Cell <marcell.made@gmail.com>
2026-09-23 22:47:29 -0400

    added and animated loading screen with custom shader and gsap

--- src/script.js
+++ src/script.js
@@ -1,12 +1,31 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { gsap } from 'gsap'
 
 /**
  * Loaders
  */
-const gltfLoader = new GLTFLoader()
-const cubeTextureLoader = new THREE.CubeTextureLoader()
+//Adding a loading manager
+const loadingManager = new THREE.LoadingManager(
+    // Loaded
+    () =>
+    {
+        // test
+        // console.log('loaded')
+        gsap.to(overlayMaterial.uniforms.uAlpha, {duration: 3, value: 0 })
+    },
+
+    // Progress
+    () =>
+    {
+        // test
+        console.log('progress')
+    }
+)
+
+const gltfLoader = new GLTFLoader(loadingManager)
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager)
 
 /**
  * Base
@@ -19,6 +38,39 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Overlay
+ */
+const overlayGeometry = new THREE.PlaneGeometry(2,2,1,1)
+// Create a basical material first to test / Test successful!
+// const overlayMaterial = new THREE.MeshBasicMaterial({color: 0xff0000})
+
+//Now create Shader material
+const overlayMaterial = new THREE.ShaderMaterial({
+    transparent: true,
+    uniforms:
+    {
+        uAlpha: {value: 1}
+    },
+    vertexShader: `
+    void main()
+    {
+    //dont apply the matirces (projection, model)
+        gl_Position = vec4(position, 1.0);
+    }
+`,
+fragmentShader: `
+    uniform float uAlpha;
+    void main()
+    {
+        gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+    }
+`
+})
+
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial)
+scene.add(overlay)
 
 /**
  * Update all materials
